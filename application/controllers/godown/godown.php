594edd9ead4ce25d962984c203ad2e3d59e2751c
Suman Mitra <sumanmitra0096@gmail.com>
2024-07-22 16:35:44 +0530

    change'

--- application/controllers/godown/godown.php
+++ application/controllers/godown/godown.php
@@ -18,10 +18,10 @@
             }
 		}
 	public function index(){
-		// $branch_id  = $this->session->userdata['loggedin']['branch_id'];
-		// $where               = array("br_id"=>$branch_id);
+		$branch_id  = $this->session->userdata['loggedin']['branch_id'];
+		$where               = array("br_id"=>$branch_id);
 
-		$data['data']   = $this->FertilizerModel->f_select('md_wearhouse',NULL,NULL,0);
+		$data['data']   = $this->FertilizerModel->f_select('md_wearhouse',NULL,$where,0);
 		$this->load->view('post_login/fertilizer_main');
 		$this->load->view("transaction/dashboard",$data);
 		$this->load->view('post_login/footer');
