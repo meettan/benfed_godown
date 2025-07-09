647e18b06b05fc26459c38da28e32ee085a9f3d3
Suman Mitra <sumanmitra0096@gmail.com>
2025-07-09 13:22:55 +0530

    chang

--- application/controllers/godown/Api.php
+++ application/controllers/godown/Api.php
@@ -82,7 +82,7 @@
            $data = json_decode($json_data, true);
            $dist_id = $data['dist_id'];
                        
-               $sql = $this->db->query("select distinct a.w_name,a.w_addrs,b.branch_name district_name,c.pres_status ,a.capacity,
+               $sql = $this->db->query("select distinct a.w_name,a.w_addrs,b.branch_name district_name,c.pres_status
                from md_wearhouse a ,md_branch b ,md_present_status c 
                where a.br_id=b.district_code and a.present_status=c.pres_status_id 
                and br_id=$dist_id");
